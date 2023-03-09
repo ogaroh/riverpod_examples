28424caae58741adeaafd855c4c0fc74452071bb
ogaroh <ogaroerick1998@gmail.com>
2023-03-09 17:45:47 +0300

    example1

---
+++ example1/android/app/src/main/kotlin/io/github/ogaroh/example1/MainActivity.kt
@@ -0,0 +1,6 @@
+package io.github.ogaroh.example1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example1/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Riverpod Test',
+      theme: ThemeData(primarySwatch: Colors.purple),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+final currentDate = Provider<DateTime>((ref) => DateTime.now());
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final date = ref.watch(currentDate);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hooks Riverpod"),
+      ),
+      body: Center(
+        child: Text(
+          date.toIso8601String(),
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+      ),
+    );
+  }
+}
